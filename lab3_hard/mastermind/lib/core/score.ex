e611e21fb72cc85a57ed2108e688155bf405b4ed
batate <bruce@rapidred.com>
2019-02-28 11:19:04 -0600

    update score

--- lab3_hard/mastermind/lib/core/score.ex
+++ lab3_hard/mastermind/lib/core/score.ex
@@ -1,21 +1,34 @@
 defmodule Mastermind.Core.Score do
   defstruct [:reds, :whites]
   
-  def new(_answer, _guess) do
+  def new(answer, guess) do
+    %__MODULE__{ 
+      reds: red_count(answer, guess), 
+      whites: white_count(answer, guess)
+    }
   end
   
-  def winner?(_score) do
+  def winner?(score) do
+    score.reds == 4
   end
   
-  def white_count(_answer, _guess) do
+  def white_count(answer, guess) do
+    length(answer) - miss_count(answer, guess) - red_count(answer, guess)
   end
     
-  def miss_count(_answer, _guess) do
+  def miss_count(answer, guess) do
+    (answer -- guess)
+    |> length
   end
   
-  def red_count(_answer, _guess) do
+  def red_count(answer, guess) do
+    answer
+    |> Enum.zip(guess)
+    |> Enum.filter(&same/1)
+    |> length
   end 
   
-  # defp same({_x, _y}) do
-  # end
+  defp same({x, y}) do
+    x == y
+  end
 end
